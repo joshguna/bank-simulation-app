37b2e599fb9c9a3e1638d7e6b268891599557662
Joshgun <joshgunf@gmail.com>
2023-03-01 21:24:26 -0500

    Components and dependency injection are implemented

--- src/main/java/com/joshguna/repository/AccountRepository.java
+++ src/main/java/com/joshguna/repository/AccountRepository.java
@@ -1,17 +1,24 @@
 package com.joshguna.repository;
 
 import com.joshguna.model.Account;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class AccountRepository {
 
+    //Group of accounts
     public static List<Account> accountList = new ArrayList<>();
 
+    //Adding new account to the group
     public Account save(Account account) {
         accountList.add(account);
         return account;
     }
 
+    public List<Account> findAll() {
+        return accountList;
+    }
 }
--- src/main/java/com/joshguna/service/impl/AccountServiceImpl.java
+++ src/main/java/com/joshguna/service/impl/AccountServiceImpl.java
@@ -4,20 +4,28 @@ import com.joshguna.enums.AccountType;
 import com.joshguna.model.Account;
 import com.joshguna.repository.AccountRepository;
 import com.joshguna.service.AccountService;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Component
 public class AccountServiceImpl implements AccountService {
 
-    AccountRepository accountRepository;
+    //whenever dependency injection, it's best to go with private final
+    private final AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
     @Override
     public Account createNewAccount(BigDecimal balance, Date creationDate, AccountType accountType, Long userID) {
 
         //1 - we need to create account object
+        //this is lombok build method, we could use setter or new keyword
         Account account = Account.builder().id(UUID.randomUUID())
                 .userId(userID)
                 .balance(balance)
@@ -32,6 +40,6 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public List<Account> listAllAccounts() {
-        return null;
+        return accountRepository.findAll();
     }
 }
