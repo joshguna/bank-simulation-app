e1e670ba7d9350798ea906ecf1051cf5e4976763
Joshgun <joshgunf@gmail.com>
2023-03-08 20:52:27 -0500

    Properties file added with under_construction parameter, updated logic

---
+++ src/main/java/com/joshguna/exception/UnderConstructionException.java
@@ -0,0 +1,7 @@
+package com.joshguna.exception;
+
+public class UnderConstructionException extends RuntimeException {
+    public UnderConstructionException(String s) {
+        super(s);
+    }
+}
--- src/main/java/com/joshguna/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/joshguna/service/impl/TransactionServiceImpl.java
@@ -4,11 +4,13 @@ import com.joshguna.enums.AccountType;
 import com.joshguna.exception.AccountOwnershipException;
 import com.joshguna.exception.BadRequestException;
 import com.joshguna.exception.BalanceNotSufficientException;
+import com.joshguna.exception.UnderConstructionException;
 import com.joshguna.model.Account;
 import com.joshguna.model.Transaction;
 import com.joshguna.repository.AccountRepository;
 import com.joshguna.repository.TransactionRepository;
 import com.joshguna.service.TransactionService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -18,6 +20,9 @@ import java.util.UUID;
 
 @Component
 public class TransactionServiceImpl implements TransactionService {
+
+    @Value("${under_construction}")
+    private boolean underConstruction;
 
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
@@ -35,15 +40,21 @@ public class TransactionServiceImpl implements TransactionService {
             -if sender has enough balance ?
             -if both accounts are checking, if not, one of them saving, it needs to be same userId
          */
-        validateAccount(sender, receiver);
-        checkAccountOwnership(sender, receiver);
-        executeBalanceAndUpdateIfRequired(amount, sender, receiver);
 
-        //after all validation and money transfer, we create Transaction obj and save/return it
-        Transaction transaction = Transaction.builder().amount(amount).sender(sender.getId())
-                .receiver(receiver.getId()).creationDate(creationDate).message(message).build();
+        if (!underConstruction) {
+            validateAccount(sender, receiver);
+            checkAccountOwnership(sender, receiver);
+            executeBalanceAndUpdateIfRequired(amount, sender, receiver);
 
-        return transactionRepository.save(transaction);
+            //after all validation and money transfer, we create Transaction obj and save/return it
+            Transaction transaction = Transaction.builder().amount(amount).sender(sender.getId())
+                    .receiver(receiver.getId()).creationDate(creationDate).message(message).build();
+
+            return transactionRepository.save(transaction);
+        } else {
+            throw new UnderConstructionException("App is under construction, try later");
+        }
+
     }
 
     private void executeBalanceAndUpdateIfRequired(BigDecimal amount, Account sender, Account receiver) {
